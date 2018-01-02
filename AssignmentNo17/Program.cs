a00810fb18e4fac8635a7314e8377af135f6518f
ahmadmohey <ahmadmohey@gmail.com>
2018-01-02 17:55:21 +0200

    Complete C# - AssignmentNo17 Solution

---
+++ AssignmentNo17/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentNo17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "lord of the rings";
+            Console.WriteLine(name.CapitalizeFirstLetter());
+        }
+    }
+
+    public static class MyExtensions
+    {
+        public static string CapitalizeFirstLetter(this string value)
+        {
+            char[] letters = value.ToLower().ToCharArray();
+            if (letters.Length >= 1)
+            {
+                if (char.IsLower(letters[0]))
+                {
+                    letters[0] = char.ToUpper(letters[0]);
+                }
+            }
+            for (int i = 1; i < letters.Length; i++)
+            {
+                if (letters[i - 1] == ' ')
+                {
+                    if (char.IsLower(letters[i]))
+                    {
+                        letters[i] = char.ToUpper(letters[i]);
+                    }
+                }
+            }
+            return new string(letters);
+        }
+    }
+}
